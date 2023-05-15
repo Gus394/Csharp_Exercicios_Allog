115bfc18029f063eb73fe90f8c29801757a1ce3f
Gustavo Coelho <88456123+Gus394@users.noreply.github.com>
2023-05-15 12:07:47 -0300

    Create Allog_04_Cliente.cs

---
+++ Allog_04_Cliente.cs
@@ -0,0 +1,18 @@
+namespace Projeto_Aula_4;
+public class Cliente
+{
+    public int id;
+    public string? nome;
+    public string? endereco;
+    public string? telefone;
+    public string? email;
+
+    /*public Cliente(int id, string nome, string endereco, string telefone, string email)
+    {
+        this.id = id;
+        this.nome = nome;
+        this.endereco = endereco;
+        this.telefone = telefone;
+        this.email = email;
+    }*/
+}

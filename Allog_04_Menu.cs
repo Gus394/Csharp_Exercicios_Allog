e863ca51f73ee89c590d27ffc5906bfd79130426
Gustavo Coelho <88456123+Gus394@users.noreply.github.com>
2023-05-15 16:22:08 -0300

    Update Allog_04_Menu.cs

--- Allog_04_Menu.cs
+++ Allog_04_Menu.cs
@@ -1,7 +1,14 @@
-namespace Projeto_Aula_4;
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 public class Menu
 {
     List<Cliente> clientes;
+    string caminho;
     int ultimo_id;
 
     public List<Cliente> Ler_arquivo(string caminho)
@@ -29,33 +36,90 @@ public class Menu
         }
 
         return clientes;
-        //clientes.FindIndex(cliente => cliente.id = 2); if -1
     }
 
-    public static Cliente Info_Cliente() {
-        Cliente cliente = new Cliente();
-        Console.WriteLine("Nome: ");
-        cliente.nome = Console.ReadLine();
-        Console.WriteLine("Endereco: ");
-        cliente.endereco = Console.ReadLine();
-        Console.WriteLine("Telefone: ");
-        cliente.telefone = Console.ReadLine();
-        Console.WriteLine("Email: ");
-        cliente.email = Console.ReadLine();
+    public static void Registrar(string caminho, List<Cliente> clientes)
+    {
+        List<string> linhas = new List<string>();
 
+        foreach (Cliente cliente in clientes)
+        {
+            string linha = string.Join(",", cliente.id, cliente.nome, cliente.endereco, cliente.telefone, cliente.email);
+            linhas.Add(linha);
+        }
+        File.WriteAllLines(caminho, linhas);
+    }
+
+    public static Cliente Info_Cliente()
+    {
+        Cliente cliente = new Cliente();
+        Console.WriteLine("Nome: ");     cliente.nome = Console.ReadLine();
+        Console.WriteLine("Endereco: "); cliente.endereco = Console.ReadLine();
+        Console.WriteLine("Telefone: "); cliente.telefone = Console.ReadLine();
+        
+        while (true) // validar e-mail
+        {
+            Console.WriteLine("Email: ");
+            cliente.telefone = Console.ReadLine();
+            Regex regex = new Regex(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$");
+            Match match = regex.Match(cliente.telefone);
+            if (match.Success)
+                break;
+            Console.WriteLine("Email invalido, tente novamente.");
+        }
+        
         return cliente;
     }
 
-    public void Cadastro(string caminho, List<Cliente> clientes) {
+    public void Cadastro()
+    {
         Cliente cliente = Info_Cliente();
         cliente.id = ultimo_id + 1;
 
-        string linha;
-
+        clientes.Add(cliente);
+        Registrar(caminho, clientes);
     }
 
-    public Menu()
+    public void Editar() // colocar try catch e verificação se -1
     {
-        clientes = Ler_arquivo("Registro_clientes.csv");
+        Console.WriteLine("Digite o ID do cliente: ");
+        int id = int.Parse(Console.ReadLine());
+        int indice;
+
+        indice = clientes.FindIndex(cliente => cliente.id == id); //if -1
+        if (indice == -1)
+            Console.WriteLine("ID não encontrado.");
+        else
+        {
+            clientes[indice] = Info_Cliente();
+            Registrar(caminho, clientes);
+        }
+    }
+
+    public void Excluir()
+    {
+        Console.WriteLine("Digite o ID do cliente a ser excluido: ");
+        int id = int.Parse(Console.ReadLine());
+        int indice;
+
+        indice = clientes.FindIndex(cliente => cliente.id == id); //if -1
+        if (indice == -1)
+            Console.WriteLine("ID não encontrado.");
+        else
+        {
+            clientes.RemoveAt(indice);
+            Registrar(caminho, clientes);
+        }
+    }
+
+    public void Listar()
+    {
+        clientes.ForEach(item => Console.Write(item.id + ", " + item.nome + ", " + item.endereco + ", " + item.telefone + ", " + item.email + "\n"));
+    }
+
+    public Menu(string caminho)
+    {
+        this.caminho = caminho;
+        clientes = Ler_arquivo(caminho);
     }
 }
